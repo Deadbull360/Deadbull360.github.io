91f00f25c20f194bba56aed83dc637b496b71ebf
Deadbull <gamerplayzone123@gmail.com>
2021-11-30 05:15:58 +0200

    figured it out xd

--- src/camera.js
+++ src/camera.js
@@ -124,8 +124,8 @@ export class Camera
       //  console.log(this.zoom);
         var zoomedin_cap = 128;
         var zoomedout_cap = 0.0625;
-        this.zoom.x = Math.min(Math.max(num, this.zoom.x), zoomedin_cap);
-        this.zoom.y = Math.min(Math.max(num, this.zoom.y), zoomedin_cap);
+        this.zoom.x = Math.min(Math.max(this.zoom.x, zoomedout_cap), zoomedin_cap);
+        this.zoom.y = Math.min(Math.max(this.zoom.y, zoomedout_cap), zoomedin_cap);
         
         for(var i = 0; i < OBJECTS.length; i++)
         {
